2352a32a6aaaec8928bf7722cc99eae6296ffa60
Yuriy Hammeke <yuriy@hammeke.de>
2021-03-29 11:07:08 +0200

    Optimized ImagePicker

--- MemeMe/MemeEditorViewController.swift
+++ MemeMe/MemeEditorViewController.swift
@@ -70,16 +70,18 @@ class MemeEditorViewController: UIViewController, UIImagePickerControllerDelegat
     // MARK: View Controller Functions
     
     @IBAction func pickAnImageFromAlbum(_ sender: Any) {
-        let imagePicker = UIImagePickerController()
-        imagePicker.delegate = self
-        imagePicker.sourceType = .photoLibrary
-        present(imagePicker, animated: true, completion: nil)
+        pickAnImage(from: .photoLibrary)
     }
     
     @IBAction func pickAnImageFromCamera(_ sender: Any) {
+        pickAnImage(from: .camera)
+    }
+    
+    // pickAnImage function provides the set up of the Image Picker from the corresponding source.
+    func pickAnImage(from source: UIImagePickerController.SourceType) {
         let imagePicker = UIImagePickerController()
         imagePicker.delegate = self
-        imagePicker.sourceType = .camera
+        imagePicker.sourceType = source
         present(imagePicker, animated: true, completion: nil)
     }
     
